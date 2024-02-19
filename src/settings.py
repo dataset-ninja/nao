a2f6dbfd64b6d299c3396f1f9bc41c983d835d15
peixed351 <peixed351@gmail.com>
2024-02-19 17:19:13 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "NAO"
 PROJECT_NAME_FULL: str = "NAO: Natural Adversarial Object Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
